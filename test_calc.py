9c68f12fe4e38fa5c2d3b5f26c8dfacbb04e23f3
JetLiTheQT <ekkachai.ittihrit2@gmail.com>
2021-04-29 16:56:32 -0700

    third commit

--- test_calc.py
+++ test_calc.py
@@ -2,16 +2,27 @@ import unittest
 import calculator
 
 class testCaseAdd(unittest.TestCase):
-    def test_add(self):
+    def test_sum(self):
         #Passing Tests
         self.assertEqual(calculator.sum(10,5), 15)
-        self.assertEqual(calculator.difference(10,5), 5)
-        self.assertEqual(calculator.quotient(10,2), 5)
-        self.assertEqual(calculator.product(2,5), 10)
-        #Failing Tests
+        #Failing Test
         self.assertEqual(calculator.sum(10,5), 12)
+        
+        #Failing Tests
+    def test_dif(self):
+        #Passing Tests
+        self.assertEqual(calculator.difference(10,5), 5)        
+        #Failing Test
         self.assertEqual(calculator.difference(10,5), 8)
+    def test_quotient(self):
+        #Passing Tests
+        self.assertEqual(calculator.quotient(10,2), 5)
+        #Failing Test
         self.assertEqual(calculator.quotient(10,2), 4)
+    def test_product(self):
+        #Passing Test
+        self.assertEqual(calculator.product(2,5), 10)
+        #Failing Test
         self.assertEqual(calculator.product(2,5), 3)
 
 
